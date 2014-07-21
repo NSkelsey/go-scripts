818c4e924b61fa98ca3d8301d0305af2d660e318
NSkelsey <nskelsey@gmail.com>
2014-07-21 13:16:44 -0400

    improved fanout, moved dirs

---
+++ fanout/main.go
@@ -0,0 +1,51 @@
+/*
+	Provides a simple command line interface to interact with a bitcoin rpc client to generate
+	valid transactions in hex format
+*/
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/NSkelsey/btcbuilder"
+)
+
+var (
+	numouts *int    = flag.Int("numout", 2, "The number of outpoints to create")
+	valout  *int    = flag.Int("valout", 110000, "The value to set at each outpoint")
+	network *string = flag.String("network", "TestNet3", "The network to use")
+	fee     *int    = flag.Int("fee", 50000, "The fee to pay for the transaction.")
+)
+
+func main() {
+	flag.Parse()
+
+	logger := log.New(os.Stdout, "", log.Ltime|log.Llongfile)
+	logger.Println(valout, valout)
+
+	params := btcbuilder.BuilderParams{
+		Fee:      int64(*fee),
+		DustAmnt: 546,
+		InTarget: int64(*valout),
+	}
+
+	btcparams, err := btcbuilder.NetParamsFromStr(*network)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	params = btcbuilder.SetParams(btcparams.Net, params)
+
+	a, _ := params.Client.GetInfo()
+	fmt.Println(a)
+	single := btcbuilder.NewPayToPubKeyHash(params, 1)
+	fmt.Println(single.Summarize())
+	list := []btcbuilder.TxBuilder{
+		single,
+	}
+	fanout := btcbuilder.NewFanOutBuilder(params, list, *numouts)
+
+	fmt.Println(btcbuilder.Send(fanout, params))
+}
