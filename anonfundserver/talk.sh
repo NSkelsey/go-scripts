8656d0f720cd01ad86d34f461038b906ad1670d8
Ubuntu <nskelsey@gmail.com>
2014-08-06 16:56:51 +0000

    insecure talk.sh

--- anonfundserver/talk.sh
+++ anonfundserver/talk.sh
@@ -1,2 +1,2 @@
 #/bin/bash
-curl --verbose --cacert cert.pem -H "Content-Type: application/json" -d '{"secret":"bilbo has the ring", "Address":"n3wDLcEM3mKoiBp9YBbfngXS2e9s1GiBWx"}' https://127.0.0.1:1050/
+curl --verbose --insecure --cacert cert.pem -H "Content-Type: application/json" -d '{"secret":"bilbo has the ring", "Address":"n3wDLcEM3mKoiBp9YBbfngXS2e9s1GiBWx"}' https://127.0.0.1:1050/
